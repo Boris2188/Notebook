a9e62255d3df9992053cd5e8f37e1b53fb6c9ae8
Badrkic Boris <zebrnjak88@mail.ru>
2025-03-08 18:32:53 +0300

    add models

--- Notebook/notebook/models.py
+++ Notebook/notebook/models.py
@@ -3,12 +3,21 @@ import sqlite3
 def creat_table(name_db: str):
     with sqlite3.connect(name_db) as conn:
         cur = conn.cursor()
-        cur.execute("""CREAT TABLE IF NOT EXESTS contacts(
-        id INTEGER PRIMARY AUTOINCREMENT,
+        cur.execute("""CREATE TABLE IF NOT EXIST contacts(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
         first_name TEXT NOT NULL,
         last_name TEXT DEFAULT NULL,
         telegram_name TEXT NOT NULL,
         vk_name TEXT DEFAULT NULL,
         phone_number TEXT DEFAULT NULL,
+        category_id INTEGER,
+        FOREIGN KEY (category_id) REFERENCES categories(id)
+        
+        )
+        """)
+
+        cur.execute("""CREATE TABLE IF NOT EXIST categories(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL UNIQUE 
         )
         """)
